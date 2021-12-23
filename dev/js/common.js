dbb82c9e5844c479f922800500d548f444758372
I <iv@MacBook-Air-I.local>
2021-12-23 07:57:54 -0800

    desktop homepage

---
+++ dev/js/common.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	initTabs()
+	initSlider()
+	initMenu()
+	initPopup()
+
+	// $("select").niceSelect()
+
+	function initSlider() {
+
+		$(".header-slider").on("init", function(event, slick) {
+
+			let buttons = $(".header-slider__dots li button");
+
+			buttons.each(function(){
+				let text = "0" + $(this).text()
+				$(this).html(text)
+			})
+
+		})
+
+		$(".header-slider").slick({
+			slidesToShow: 1,
+			fade: true,
+			speed: 1000,
+			arrows: false,
+			dots: true,
+			dotsClass: "header-slider__dots"
+		})
+
+	}
+
+	function initMenu() {
+
+		let submenuTrigg = $(".header-menu .has-submenu")
+		let submenu = $(".header-menu__submenu")
+
+		submenuTrigg.on("click", function(){
+			if($(this).hasClass("active")) {
+				$(this).removeClass("active")
+				submenu.fadeOut(500)
+			} else {
+				$(this).addClass("active")
+				submenu.fadeIn(500)
+			}
+		})
+
+	}
+
+	function initTabs() {
+		let tabsLink =	$(".tabs__nav .tab-link")
+		let tabs = $(".tabs .tab")
+		tabsLink.on("click", function(){
+			let num = $(this).data("tab")
+			$(this).parent().find(".active").removeClass("active")
+			$(this).parent().parent().find(".tab.active").fadeOut(0).removeClass("active")
+			$(this).addClass("active")
+			$(this).parent().parent().find(".tab[data-tab-item='" + num + "']").fadeIn(400).addClass("active")
+		})
+	}
+
+	function initPopup() {
+		
+		$('.open-popup-link').magnificPopup({
+			type:'inline',
+			midClick: true
+		})
+	}
+})
---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+const { src, dest, parallel, series, watch } = require('gulp')
+const browserSync = require('browser-sync').create()
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify-es').default
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleancss = require('gulp-clean-css')
+const sprite = require('gulp-svg-sprite')
+const rename = require('gulp-rename')
+
+function bsync() {
+  browserSync.init({
+    server: {
+      baseDir: 'dev',
+      index: 'index.html'
+    },
+    notify: false,
+    online: true
+  })
+}
+
+function styles() {
+  return src('dev/scss/**/*.scss')
+         .pipe(sass().on('error', sass.logError))
+         .pipe(autoprefixer({
+            overrideBrowserlist: ['last 10 versions'],
+            grid: true
+          }))
+         .pipe(cleancss({
+           level: {
+             1: {
+               specialComments: 0
+             }
+           },
+          //  format: 'beautify'
+         }))
+         .pipe(rename('main.min.css'))
+         .pipe(dest('dev/css/'))
+         .pipe(browserSync.stream())
+}
+
+function scripts() {
+  return src([
+        'dev/libs/jquery/jquery-3.6.0.min.js',
+        'dev/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+        'dev/libs/slick-carousel/slick/slick.min.js',
+        'dev/libs/jquery-nice-select/js/jquery.nice-select.min.js',
+        'dev/js/common.js'
+          ])
+         .pipe(concat('app.min.js'))
+         .pipe(uglify())
+         .pipe(dest('dev/js/'))
+         .pipe(browserSync.stream())
+}
+
+function svgsprite() {
+  return src('dev/svg/src/**/*.svg')
+         .pipe(sprite({
+            mode: {
+              stack: {
+                  sprite: 'sprite.svg'  // sprite file name
+              }
+            },
+         }))
+         .pipe(dest('dev/svg/dest'))
+}
+
+function build() {
+  return src([
+            'dev/css/**/*.min.css',
+            'dev/js/**/*.min.js',
+            'dev/images/**/*',
+            'dev/svg/**/*',
+            'dev/fonts/**/*',
+            'dev/*.html'
+          ], { base: 'dev' })
+         .pipe(dest('build'))
+}
+
+function startwatch() {
+  watch(['dev/js/**/*.js', '!dev/js/*.min.js'], scripts) // exclude main file and stop recursive watching
+  watch('dev/scss/**/*.scss', styles)
+  watch('dev/images/src/**/*', browserSync.reload)
+  watch('dev/svg/src/**/*', svgsprite)
+  watch('dev/*.html').on('change', browserSync.reload)
+}
+
+exports.bsync = bsync
+exports.styles = styles
+exports.scripts = scripts
+exports.startwatch = startwatch
+exports.svgsprite = svgsprite
+exports.build = series(styles, scripts, build)
+
+// --- //
+
+exports.default = parallel(styles, scripts, bsync, startwatch)
